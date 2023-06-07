6cf65822a693bbbb1e5db15e61be07474627b1d7
Bohdan Savastieiev <bohdan.savastieiev.it@gmail.com>
2023-06-07 18:38:41 +0300

    UseDeveloperExceptionPage

--- DictionaryApplication/Program.cs
+++ DictionaryApplication/Program.cs
@@ -78,17 +78,20 @@ using (var scope = app.Services.CreateScope())
     SeedData.Initialize(context);
 }
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+app.UseDeveloperExceptionPage();
+app.UseHsts();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseMigrationsEndPoint();
+//}
+//else
+//{
+//    app.UseExceptionHandler("/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
